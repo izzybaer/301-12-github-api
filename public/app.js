3accd18d120c3b5dde921501e009589c840b70f2
Izabella Arya <izzy@izzys-magic-box.local>
2017-05-23 11:59:05 -0700

    done

--- public/app.js
+++ public/app.js
@@ -1,8 +1,18 @@
 // Let's make an AJAX call to the GitHub API and then do a simple render of the data into the DOM
+'use strict';
 
 $.ajax({
   // go get the data
+  url: 'https://api.github.com/user/repos',
+  method: 'GET',
+  headers: {
+    Authorization: `token ${myDamnGitHubToken}`
+  }
 })
 .then(
   // render the data
+  data => {
+    data.forEach(repo =>
+       $('#results').append(`<p>${repo.name}</p>`))
+  }
 );
